d41ddaeb6ab223f6035a6e0b612ec50ad456da8b
Ajayprajapati786 <prajapatisani9@gmail.com>
2020-09-20 18:07:45 +0530

    to upload

---
+++ js/flower.js
@@ -0,0 +1,49 @@
+
+  var randomNumber = Math.ceil(Math.random()*200);
+  console.log(randomNumber);
+
+async function addImageToDOM() {
+  
+  let url = `https://api.unsplash.com/search/photos/?client_id=92NZFIvM1iy3iWidpjahP7POR7_sC-AEav3mH4sDNUs&query=flowers&page=${randomNumber}&per_page=30`;
+
+     fetch(url)
+      .then(function(response){
+           return response.json();
+      })
+      .then(function(data){
+          //  console.log(data);
+           data.results.forEach(function(photo){
+           let result = photo.urls.regular;
+           const imageDiv = document.createElement('div');
+           imageDiv.className = 'one-fourth';
+           const imgElement = document.createElement('img');
+           imgElement.src = generateImageLinks();
+           imageDiv.append(imgElement);
+           document.querySelector('.container').append(imageDiv);
+           function generateImageLinks() {
+            return result;
+          }
+           });
+
+      })
+
+     }
+
+function fetchRandomImages(number) {
+  for (var i = 0; i < number; i++) {
+    addImageToDOM();
+  }
+}
+
+fetchRandomImages(30);
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY + window.innerHeight + 100 >= document.documentElement.scrollHeight) {
+    fetchRandomImages(50);
+    
+  }
+})
+
+
+
+
